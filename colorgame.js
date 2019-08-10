1304a1939b5b9a42efd75f438f40852841c16fff
amanarts <amanarts007@gmail.com>
2019-08-10 23:19:14 +0530

    Added function generateRandomColors and randomColor for six different squares

--- colorgame.js
+++ colorgame.js
@@ -1,12 +1,5 @@
-var colors=[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-    ]
-
+var colors=generateRandomColors(6);
+var h1=document.querySelector("h1");
 var squares=document.querySelectorAll(".square");
 var pickedColor=pickColor();
 var colorDisplay=document.getElementById("colorDisplay");
@@ -28,6 +21,7 @@ for(var i=0;i<squares.length;i++)
         {
             messageDisplay.textContent="Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor=clickedColor;
         }
         else
         {
@@ -50,5 +44,29 @@ function pickColor()
 {
     var random=Math.floor(Math.random()*colors.length);
     return colors[random];
+
+}
+
+function generateRandomColors(num)
+{
+    //make an array
+    var arr=[];
+    //repeat num times
+    for(var i=0;i<num;i++)
+    {
+        //get random color and push it into array
+        arr.push(randomColor());
+
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor()
+{
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return "rgb("+r+", "+g+", "+b+")";
 
 }
